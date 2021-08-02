9d654d3990b6e94869582dd771656fc56b2c1f5d
snake_spelt_backward_Ben-S <84112591+ben-S-lgtm@users.noreply.github.com>
2021-08-02 18:27:37 +1000

    (snake) timetable - html pages, flask links
    added flask links for details, home, login
    added some css, navbar for the entire application
    
    just need to add/change links to homepage links. have fun fellas :)

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template()
+  return render_template("login.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/detail')
+def detail():
+    return render_template('details.html')
+
 
 if __name__ == '__main__':
    app.run(debug = True)
